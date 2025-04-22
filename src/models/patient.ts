98aa386b3305c2ffacf3141ea58d844b363689e3
Samuel <samuel.profissional18@gmail.com>
2025-04-21 22:01:01 -0300

    Update patient.ts

--- src/models/patient.ts
+++ src/models/patient.ts
@@ -1,4 +1,4 @@
-export type Status = 'In triage queue'| 'In consult queue' | 'In consult' | 'Was treated'
+export type Status = 'In triage queue'| 'In consult queue' | 'In consult' | 'Was treated' | 'Left before consult'
 export type Severity = 'Non-urgent' | 'Low-urgency' | 'Urgent' | 'Very-urgent' | 'Immediate'
 export type MaritalStatus = 'Single' | 'Married' | 'Divorcied' | 'Separated'
 export type Gender = 'Male' | 'Female' | 'Other'

67d0585b5a994842936c64febacd859f7b197850
Zidan-09 <samuel.profissional18@gmail.com>
2025-05-13 16:51:23 -0300

    Tem que arrumar o cadastro de pacientes

--- backend/src/services/hospitalService.ts
+++ backend/src/services/hospitalService.ts
@@ -15,7 +15,7 @@ export class HospitalServices {
         // const patient: any = await db.execute('INSERT INTO Patients (name, dob, maritalStatus, cpf, rg, contacts, gender, healthPlan, address) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', MySQL
         //     [data.name, data.dob, data.maritalStatus, data.cpf, data.rg, data.contact, data.gender, data.healthPlan, data.address]
         // );
-        const patient = (await db).run('INSERT INTO Patients (name, dob, maritalStatus, cpf, rg, contacts, gender, healthPlan, address) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [data.name, data.dob, data.maritalStatus, data.cpf, data.rg, data.contact, data.gender, data.healthPlan, data.address])
+        const patient = (await db).run('INSERT INTO Patient (name, dob, maritalStatus, cpf, rg, contact, gender, healthPlan, address) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [data.name, data.dob, data.maritalStatus, data.cpf, data.rg, data.contact, data.gender, data.healthPlan, data.address])
         const patient_id = (await patient).lastID;
         const nodeTriage: NodeTriage = new NodeTriage(patient_id!);
         InsertQueue.insertTriageQueue(nodeTriage);
@@ -25,7 +25,7 @@ export class HospitalServices {
 
     static async triage(data: Triage) {
         // const [result]: any = await db.execute('INSERT INTO Triages (nurse_id, patient_id, vitalSigns, simptoms, painLevel, triageCategory) VALUES (?, ?, ?, ?, ?, ?)', [data.nurse_id, data.patient_id, data.vitalSigns, data.simptoms, data.painLevel, data.triageCategory]); MySQL
-        const [result]: any = (await db).run('INSERT INTO Triages (nurse_id, patient_id, vitalSigns, simptoms, painLevel, triageCategory) VALUES (?, ?, ?, ?, ?, ?)', [data.nurse_id, data.patient_id, data.vitalSigns, data.simptoms, data.painLevel, data.triageCategory])
+        const [result]: any = (await db).run('INSERT INTO Triage (nurse_id, patient_id, vitalSigns, simptoms, painLevel, triageCategory) VALUES (?, ?, ?, ?, ?, ?)', [data.nurse_id, data.patient_id, data.vitalSigns, data.simptoms, data.painLevel, data.triageCategory])
         const no: NodeConsult = new NodeConsult(data);
         InsertQueue.insertConsultQueue(no);
         return result;
@@ -82,14 +82,14 @@ export class HospitalServices {
     static async startConsult(data: StartConsult): Promise<number> {
         const consult: Consult = new Consult(data.doctor_id, 1); // Tirar o 1 e botar patient_id
         // const [result]: any = await db.execute('INSERT INTO Consults (patient_id, doctor_id, checkInConsult) VALUES (?, ?, NOW())', [consult.patient_id, consult.doctor_id]); MySQL
-        const [result]: any = (await db).run(`INSERT INTO Consults (patient_id, doctor_id, checkInConsult) VALUES (?, ?, datetime('now'))`, [consult.patient_id, consult.doctor_id]);
+        const [result]: any = (await db).run(`INSERT INTO Consult (patient_id, doctor_id, checkInConsult) VALUES (?, ?, datetime('now'))`, [consult.patient_id, consult.doctor_id]);
         const consult_id: number = result.lastID;
         return consult_id
     };
 
     static async endConsult(data: EndConsult) {
         // const consult: any = db.query('SELECT * FROM Consults WHERE id = ?', [data.id]); MySQL
-        const consult: any = (await db).get('SELECT * FROM Consults WHERE id = ?', [data.id]);
+        const consult: any = (await db).get('SELECT * FROM Consult WHERE id = ?', [data.id]);
         consult.checkOutConsult = new Date();
         consult.diagnosis = data.diagnosis;
         consult.prescriptions = data.prescriptions;

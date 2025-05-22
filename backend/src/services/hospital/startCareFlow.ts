c219e83fc872d51c1d4fefb808b36345cae2a371
Zidan-09 <samuel.profissional18@gmail.com>
2025-05-22 17:23:28 -0300

    DEU CERTO PORRAAAAAAAAAAAAAAAA, Tem que fazer cadastro dos empregado agora

--- backend/src/services/hospital/startCareFlow.ts
+++ backend/src/services/hospital/startCareFlow.ts
@@ -5,7 +5,7 @@ const db = openDb();
 
 export class CareFlowService {
     static async startCareFlow(patient_id: number, data: CareFlow): Promise<number> {
-        const careFlow = (await db).run(`INSERT INTO CareFlow (patient_id, receptionist_id, status, time) VALUES (?, ?, ?, datetime('now'))`, [patient_id, data.receptionist_id, 'In queue']);
+        const careFlow = (await db).run(`INSERT INTO CareFlow (patient_id, receptionist_id, status, date) VALUES (?, ?, ?, datetime('now'))`, [patient_id, data.receptionist_id, 'In queue']);
         const careFlowId: number = (await careFlow).lastID!
         return careFlowId;
     }

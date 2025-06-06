5c3f1aa1f611836e895a6cc5b6ba32bbea5478ba
Gabriel Lima <gabriellimatrabalho1@gmail.com>
2025-06-06 11:59:03 -0300

    Tela_Recepicionista

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100 p-8">
       <h1 className="text-3xl font-bold text-blue-600">
-        Next.js 14 + Tailwind 🚀
+        Gerenciamento de Filas em Pronto-Socorro
       </h1>
     </main>
   )

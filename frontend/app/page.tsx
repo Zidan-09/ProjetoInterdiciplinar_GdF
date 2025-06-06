d5a5945fccc5a74f8ca46d24f8fe976991a96fed
Gabriel Lima <gabriellimatrabalho1@gmail.com>
2025-06-06 10:31:58 -0300

    Iniciando o front

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Next.js 14 + Tailwind 🚀
+      </h1>
+    </main>
+  )
+}

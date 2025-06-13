3b40617227dd89c15bd5ca6697c2fbcb47a41ffb
Zidan-09 <samuel.profissional18@gmail.com>
2025-06-13 02:26:07 -0300

    Banco de dados MySQL Integrado... VAMOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO, ACABOU ESSA JOÇAAAAAAAAAAAAA!!!!!!!!!!!!!!!!!!!

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,12 +2,13 @@ import mysql from 'mysql2/promise';
 require('dotenv').config();
 
 export const db = mysql.createPool({
-    host: process.env.DB_HOST! || "192.168.5.21",
-    user: process.env.DB_USER! || 'api',
-    password: process.env.DB_PASSWORD! || 'J7#kP9$mX!2L*5zQ',
-    database: process.env.DB_DATABASE! || 'ProjetoInter',
-    port: Number(process.env.DB_PORT!) || 3306,
+    host: process.env.DB_HOST!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_DATABASE!,
+    port: Number(process.env.DB_PORT!),
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    socketPath: undefined
 })

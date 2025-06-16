8024b89b18a9bafb6277a76e60892df234c913a1
Zidan-09 <samuel.profissional18@gmail.com>
2025-06-16 17:03:02 -0300

    teste

--- backend/src/services/adm/employeeLogin.ts
+++ backend/src/services/adm/employeeLogin.ts
@@ -12,7 +12,7 @@ export const Login = {
             const [userData] = await db.execute<RowDataPacket[]>('SELECT * FROM User WHERE username = ?', [data.username]);
             const [role] = await db.execute<RowDataPacket[]>('SELECT accessLevel FROM Employee WHERE id = ?', [userData[0].user_id])
 
-            if (userData.length) {
+            if (userData.length > 0) {
                 const valid: boolean = await bcrypt.compare(data.password, userData[0].password);
 
                 if (valid) {

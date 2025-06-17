3fe738953dc36e614252ac5205b4639f76371890
Zidan-09 <samuel.profissional18@gmail.com>
2025-06-17 10:08:06 -0300

    Chamados no novo front

--- frontend3/app/admin/admin.tsx
+++ frontend3/app/admin/admin.tsx
@@ -1,4 +1,14 @@
+import { useRouter } from 'next/router';
+
 export default function AdminPage() {
-    return <h1>Painel do Admin</h1>;
-  }
-  
+  const router = useRouter();
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Painel do Admin</h1>
+      <button onClick={() => router.push('/calls')} style={{ marginTop: 20 }}>
+        Ver Chamados
+      </button>
+    </div>
+  );
+}

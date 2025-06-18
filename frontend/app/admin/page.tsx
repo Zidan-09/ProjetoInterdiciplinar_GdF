58073937bd21689dc55fb48fc6aacebfdfce4b21
Gabriel Lima <gabriellimatrabalho1@gmail.com>
2025-06-18 13:43:23 -0300

    Cadastro de funcionario

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -1,8 +1,15 @@
 'use client';
 import { useAuth } from '../utils/authRedirect';
+import RegisterForm from './components/registerForm';
 
 export default function AdminPage() {
   useAuth('admin');
 
-  return <h1>Bem-vindo ao Painel do Administrador</h1>;
+  return (
+    <div className='p-6'>
+      <h1>Bem-vindo ao Painel do Administrador</h1>
+      <h2 className='text-xl font-bold mb-4'>Painel do admin</h2>
+      <RegisterForm/>
+    </div>
+  );
 }

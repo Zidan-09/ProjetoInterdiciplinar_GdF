901edd749a5c36501ccd2054d4e13b62e213d5d9
Gabriel Lima <gabriellimatrabalho1@gmail.com>
2025-06-19 21:11:22 -0300

    Recepcionista ok

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,17 @@
+'use client';
 import React from 'react';
+import { useRouter } from 'next/navigation';
 
 const HomePage: React.FC = () => {
+  const router = useRouter();
+
+  const handleLogin = () => {
+    router.push('/login');
+  };
+
   return (
     <div
-      className="relative flex items-center justify-center min-h-screen w-full"
+      className="relative min-h-screen w-full"  // Removido flex-center
       style={{ 
         backgroundImage: "url('/AlbedoBase_XL_abstract_background_with_light_bluelight_aqua_gr_3.jpg')",
         backgroundSize: 'cover',
@@ -12,15 +20,24 @@ const HomePage: React.FC = () => {
         backgroundAttachment: 'fixed'
       }}
     >
-      <div className="absolute inset-0 bg-black opacity-50"></div>
-
-      <div className="relative z-10 text-center text-white p-6 rounded-lg bg-gray-800 bg-opacity-60">
-        <h1 className="text-5xl font-bold mb-4">
-          Bem-vindo ao Meu Projeto Next.js!
-        </h1>
-        <p className="text-xl">
-          Esta é uma tela inicial simples e elegante.
-        </p>
+      <div className="w-full max-w-6xl px-6 text-white pb-70 pl-10">
+        <div className="mb-3">
+          <h1 className="text-6xl font-bold mb-2 transform translate-y-50">
+            SISTEMA DE GERENCIAMENTO DE<br />
+            <span className="block mt-10">FILAS EM PRONTO-SOCORRO</span>
+          </h1>
+          <p className="text-xl mb-8">
+            Em situações de emergência, ninguém deveria esperar. Nosso sistema transforma o caos<br />
+            <span className='block mt-0'>das filas de pronto-socorro em um fluxo organizado, justo e eficiente</span>
+          </p>
+          
+          <button
+            onClick={handleLogin}
+            className="gap-2 bg-white/20 hover:bg-white/30 text-white font-semibold text-lg py-4 px-8 rounded-lg backdrop-blur-sm border border-white/30 transition-all duration-300 hover:shadow-lg hover:scale-105"
+          >
+            Acessar o Sistema
+          </button>
+        </div>
       </div>
     </div>
   );

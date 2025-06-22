8bdf56f80b64c3a465c0c05a66235d151c0c03e5
Gabriel Lima <gabriellimatrabalho1@gmail.com>
2025-06-22 14:05:14 -0300

    ...

--- frontend/app/doctor/page.tsx
+++ frontend/app/doctor/page.tsx
@@ -1,8 +1,12 @@
 'use client';
 import { useAuth } from '../utils/authRedirect';
+import { useEffect,useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function DoctorPage() {
   useAuth('doctor');
+
+  const router = useRouter();
 
   return <h1>Bem-vindo ao Painel da Gokuzão</h1>;
 }

b13dcfd67b4d947c6f44eb666d982fb887a796b2
Zidan-09 <samuel.profissional18@gmail.com>
2025-06-27 01:50:19 -0300

    Vamooooooooooooo

--- backend/src/router/admin.routes.ts
+++ backend/src/router/admin.routes.ts
@@ -8,7 +8,7 @@ const adminRouter: Router = Router();
 
 adminRouter.get('/triageCategory/list', loginVerify, AccessLevelMiddleware.admin, AdminController.listTriageCategories);
 adminRouter.post('/triageCategory/create', loginVerify, AccessLevelMiddleware.admin, AdminController.createTriageCategory);
-adminRouter.put('/triageCategory/edit', loginVerify, AccessLevelMiddleware.admin, AdminController.updateTriageCategory);
+adminRouter.patch('/triageCategory/edit', loginVerify, AccessLevelMiddleware.admin, AdminController.updateTriageCategory);
 adminRouter.delete('/triageCategory/delete/byName/:triageCategory', loginVerify, AccessLevelMiddleware.admin, passwordVerify, AdminController.deleteTriageCategory);
 
 adminRouter.get('/careFlow/list', loginVerify, AccessLevelMiddleware.admin, AdminController.listCareFlows);
